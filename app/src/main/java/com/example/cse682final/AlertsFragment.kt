23186763223daae8687565ef9df772ac89194653
Etienne Begert <3597381+etbegert@users.noreply.github.com>
2020-09-02 11:41:47 -0700

    Added AlertsFragment.kt and alerts_fragment.xml, set up basic layout.

--- app/src/main/java/com/example/cse682final/AlertsFragment.kt
+++ app/src/main/java/com/example/cse682final/AlertsFragment.kt
@@ -6,13 +6,12 @@ import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
 
-class ExpenditureFragment : Fragment() {
+class AlertsFragment : Fragment() {
     override fun onCreateView(
         inflater: LayoutInflater,
         container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        return inflater.inflate(R.layout.expenditure_fragment, container)
+        return inflater.inflate(R.layout.alerts_fragment, container)
     }
-
 }
